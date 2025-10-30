3761b80c90be9d8aa23b917cb258f52592647b59
subani shaik <144584390+subani248@users.noreply.github.com>
2025-10-30 23:14:09 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://student-attendence-backend1.onrender.com/api';
+const API_URL = 'https://student-attendence-backend1.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_URL,
@@ -24,4 +24,5 @@ api.interceptors.request.use(
 );
 
 export default api;
+
 
